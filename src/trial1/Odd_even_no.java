87c450b0ee5b7a60cbd216bb53b3f29b3b4cc92b
Ankush <Ankush@192.168.1.9>
2022-11-15 16:07:27 +0530

    hii

---
+++ src/trial1/Odd_even_no.java
@@ -0,0 +1,20 @@
+package trial1;
+
+public class Odd_even_no {
+public static void main(String[] args) {
+	//for(int i = 1;i<=10; i++) {
+	int i =1;
+	while(100>i) {
+		
+	if (i%2==0)
+		System.out.println(i++ + "even no");
+		else
+			System.out.println(i++ + " odd no");
+	
+		}
+	/*for (int i = 1; i<=10; i++) {
+		System.out.println(i++);
+	}*/
+
+}
+}
